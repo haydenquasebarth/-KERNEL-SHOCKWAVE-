9b7f1caa33b36d28c654ef7be62555df158ca5f7
----idk---- <166407565+haydenquasebarth@users.noreply.github.com>
2025-01-27 16:48:57 -0500

    Add files via upload

---
+++ PASWORD.js
@@ -0,0 +1,6 @@
+const users = [
+    { username: "admin", password: "admin123" },
+    { username: "student1", password: "mathrocks" },
+    { username: "teacher1", password: "teach2025" }
+  ];
+  
